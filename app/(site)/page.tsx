6b229b34b3dac7e35799540f1a416b0ea5f38e68
radekmikulik <radek.mikulik@gmail.com>
2025-11-06 12:05:15 +0100

    Update page.tsx

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,9 +1,5 @@
 // app/(site)/page.tsx
-import HomeBaseline from "./_components/HomeBaseline";
-
-export const dynamic = "force-static";
-export const fetchCache = "force-cache";
-// volitelné: export const revalidate = false;
+import HomeBaseline from './_components/HomeBaseline';
 
 export default function Page() {
   return <HomeBaseline />;
